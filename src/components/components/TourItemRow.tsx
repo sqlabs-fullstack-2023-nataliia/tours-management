c028953c1b60d8b0005049db4a626afe57916f04
Nataliia Stasiukova <nstasiukova@gmail.com>
2024-03-18 02:06:47 +0200

    small fixes

--- src/components/components/TourItemRow.tsx
+++ src/components/components/TourItemRow.tsx
@@ -1,4 +1,4 @@
-import { TourModel } from '../../models/TourModel'
+import { TourItemView } from '../../models/TourItemView'
 
 // TODO take status from firebase + mb add color picker to settings
 const statusColor: { [key: string]: string } = {
@@ -11,32 +11,30 @@ const statusColor: { [key: string]: string } = {
 }
 
 interface Props {
-    tours: TourModel[]
+    tourItemsView: TourItemView[]
 }
 
-const TourItemRow = ({tours}: Props) => {
-
-    //const tours = useTourStore((state) => state.tours)
+const TourItemRow = ({tourItemsView}: Props) => {
     
   return (
     <div >
-    {
-        tours.map((tour, index) => {
-            return tour.tourItems.map((tourItem, i) => {
-                return <div className="row p-2" key={tourItem.id} style={{ background: statusColor[tourItem.status] }}>
-                <div className="col col-2" style={{fontWeight: 'bold', color: 'rgb(44, 48, 53)'}}>{tour.name}</div>
-                <div className="col col-1" style={{fontWeight: 'bold', color: 'rgb(44, 48, 53)'}}>
-                    <img src={tour.image} rel='image' style={{ width: '100%' }}/>
-                </div>
-                <div className="col col-2" style={{fontWeight: 'bold', color: 'rgb(44, 48, 53)'}}>{tour.destination}</div>
-                <div className="col col-2" style={{fontWeight: 'bold', color: 'rgb(44, 48, 53)'}}>{tour.duration}</div>
-                <div className="col col-2" style={{fontWeight: 'bold', color: 'rgb(44, 48, 53)'}}>{tourItem.departureDate}</div>
-                <div className="col col-2" style={{fontWeight: 'bold', color: 'rgb(44, 48, 53)'}}>{tourItem.status}</div>
-                <div className="col col-1" style={{fontWeight: 'bold', color: 'rgb(44, 48, 53)'}}>{tourItem.availability}/{tourItem.totalAvailability}</div>
-              </div>
-            })
+      {
+        tourItemsView.map((item, index) => {
+          return <div className="row p-2" key={item.id} style={{ background: statusColor[item.status] }}>
+          <div className="col col-1" style={{fontWeight: 'bold', color: 'rgb(44, 48, 53)'}}>{item.name}</div>
+          <div className="col col-1" style={{fontWeight: 'bold', color: 'rgb(44, 48, 53)'}}>
+              <img src={item.image} rel='image' style={{ width: '100%' }}/>
+          </div>
+          <div className="col col-2" style={{fontWeight: 'bold', color: 'rgb(44, 48, 53)'}}>{item.language}</div>
+          <div className="col col-1" style={{fontWeight: 'bold', color: 'rgb(44, 48, 53)'}}>{item.duration}</div>
+          <div className="col col-2" style={{fontWeight: 'bold', color: 'rgb(44, 48, 53)'}}>{item.destination}</div>
+          <div className="col col-2" style={{fontWeight: 'bold', color: 'rgb(44, 48, 53)'}}>{item.departureDate}</div>
+          <div className="col col-1" style={{fontWeight: 'bold', color: 'rgb(44, 48, 53)'}}>{item.status}</div>
+          <div className="col col-2 ps-5" style={{fontWeight: 'bold', color: 'rgb(44, 48, 53)'}}>{item.availability}/{item.totalAvailability}</div>
+
+        </div>
         })
-    }
+      }
     </div>
   )
 }
