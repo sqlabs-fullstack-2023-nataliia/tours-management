d7f0cfbaab17d8ef3fd40fa4894f65d7547f4d7a
Nataliia Stasiukova <nstasiukova@gmail.com>
2024-03-26 01:06:35 +0200

    adding responcivety to tours page and tour form for items

--- src/components/components/tour/TourItemsRow.tsx
+++ src/components/components/tour/TourItemsRow.tsx
@@ -1,6 +1,8 @@
 import { TbPencil } from "react-icons/tb";
 import { FaRegTrashAlt } from "react-icons/fa";
 import { useTourItemStore } from "../../../store/useTourItemStore";
+import { TourItemModel } from "../../../models/TourItemModel";
+import { useState } from "react";
 
 // TODO take status from firebase + mb add color picker to settings
 const statusColor: { [key: string]: string } = {
@@ -12,39 +14,45 @@ const statusColor: { [key: string]: string } = {
 }
 
 interface Props {
+  tourItemsView?: TourItemModel[],
   viewMode?: boolean
 }
 
-const TourItemRow = ({viewMode}: Props) => {
+const TourItemRow = ({viewMode, tourItemsView}: Props) => {
 
   const tourItems = useTourItemStore((state) => state.tourItems)
   const setTourItem = useTourItemStore((state) => state.setTourItem)
   const removeTourItem = useTourItemStore((state) => state.deleteTourItem)
+  const [curTourItems, setCurTourItems] = useState<TourItemModel[]>(!!tourItemsView ? [...tourItemsView] : [...tourItems])
 
   return (
     <div className="container" >
       {
-        tourItems.length === 0
+        curTourItems.length === 0
           ? (<div className='d-flex justify-content-center ' ><h3>No tour options found</h3></div>)
           : (<div className="container-fluid" >
             <div className="row py-3 mb-2 px-2">
-              <div className="col col-md-2" style={{ fontWeight: 'bold', color: 'rgb(44, 48, 53)' }}>#</div>
-              <div className="col col-2 d-none d-md-block" style={{ fontWeight: 'bold', color: 'rgb(44, 48, 53)' }}>Departure</div>
-              <div className="col col-md-2" style={{ fontWeight: 'bold', color: 'rgb(44, 48, 53)' }}>Language</div>
-              <div className="col col-md-2" style={{ fontWeight: 'bold', color: 'rgb(44, 48, 53)' }}>Availability</div>
-              <div className="col col-2 d-none d-md-block" style={{ fontWeight: 'bold', color: 'rgb(44, 48, 53)' }}>Price</div>
-              <div className="col col-md-2" style={{ fontWeight: 'bold', color: 'rgb(44, 48, 53)' }}>Status</div>
+              <div className="col col-lg-2" style={{ fontWeight: 'bold', color: 'rgb(44, 48, 53)' }}>ID</div>
+              <div className={`col col-2 d-none d-${!!viewMode ? 'lg' : 'lg'}-block`} style={{ fontWeight: 'bold', color: 'rgb(44, 48, 53)' }}>Departure</div>
+              <div className={`col col-xl-${!!viewMode ? '2' : '1'} col-2`} style={{ fontWeight: 'bold', color: 'rgb(44, 48, 53)' }}>Language</div>
+              <div className={`col col-lg-2`} style={{ fontWeight: 'bold', color: 'rgb(44, 48, 53)' }}>Status</div>
+              <div className={`col col-lg-2`} style={{ fontWeight: 'bold', color: 'rgb(44, 48, 53)' }}>Availability</div>
+              <div className={`col d-none d-${!!viewMode ? 'md' : 'xl'}-block`} style={{ fontWeight: 'bold', color: 'rgb(44, 48, 53)' }}>Price</div>
+              {/* <div className={`col col-lg-2`} style={{ fontWeight: 'bold', color: 'rgb(44, 48, 53)' }}>Status</div> */}
+              {!!!viewMode && <div className="col col-lg-2"></div>}
+              
             </div>
             <div className='p-4' style={{ background: 'white', borderRadius: '15px' }}>
               {
-                tourItems.map((tourItem, index) => {
+                curTourItems.map((tourItem, index) => {
                   return <div className="row" key={index} style={{ background: statusColor[tourItem.status] }}>
-                    <div className="col col-md-2 p-2">{index + 1}</div>
-                    <div className="col col-2 p-2 d-none d-md-block">{tourItem.departureDate}</div>
-                    <div className="col col-md-2 p-2">{tourItem.language}</div>
-                    <div className="col col-md-2 p-2">{tourItem.availability}</div>
-                    <div className="col col-2 p-2 d-none d-md-block">{tourItem.price}</div>
-                    <div className="col col-md-2 p-2">{tourItem.status}</div>
+                    <div className="col col-lg-2 pt-2">{index + 1}</div>
+                    <div className={`col col-2 pt-2 d-none d-${!!viewMode ? 'lg' : 'lg'}-block`}>{tourItem.departureDate}</div>
+                    <div className={`col pt-2 col-xl-${!!viewMode ? '2' : '1'} col-2`}>{tourItem.language}</div>
+                    <div className={`col col-lg-2 pt-2`}>{tourItem.status}</div>
+                    <div className={`col col-lg-2 pt-2`}>{tourItem.availability}</div>
+                    <div className={`col pt-2 d-none d-${!!viewMode ? 'md' : 'xl'}-block`}>{tourItem.price}</div>
+                    {/* <div className={`col col-lg-2 pt-2`}>{tourItem.status}</div> */}
                     {
                       !!!viewMode && <>
                       <div className="col col-1 p-2">
--- src/components/pages/tour/Tours.tsx
+++ src/components/pages/tour/Tours.tsx
@@ -1,28 +1,85 @@
 import TourRow from '../../components/tour/TourRow';
 import { useTourStore } from '../../../store/useTourStore';
+import { IoIosArrowRoundUp } from "react-icons/io";
+import { IoIosArrowRoundDown } from "react-icons/io";
+import { useState } from 'react';
+import { TourModel } from '../../../models/TourModel';
+
+const STRING_TYPE = 'string'
+const NUMBER_TYPE = 'number'
+const DESCENDING_ORDER  = 'descending'
+const ASCENDING_ORDER = 'ascending'
+const MIN_DATE_VALUE = '2023-12-31'
+const MAX_DATE_VALUE = new Date(`${new Date().getFullYear() + 2}-12-31`).toISOString().split("T")[0]
 
 const Tours = () => {
 
+  const [id, setId] = useState('');
+  const [date, setDate] = useState('')
+  const [days, setDays] = useState('')
+  const [name, setName] = useState('')
+  const [status, setStatus] = useState('')
+  const [language, setLanguage] = useState('')
+  const [duration, setDuration] = useState('')
+  const [destination, setDestination] = useState('')
+
   const tours = useTourStore((state) => state.tours)
+  const [curTours, setCurTours] = useState<TourModel[]>([...tours])
+
+  const handleSort = (filter: string, type: string, order: string) => {
+    const sortedTours = type === 'string' 
+      ? [...tours].sort((a: any, b: any) => order === ASCENDING_ORDER ? a[filter].localeCompare(b[filter]) : b[filter].localeCompare(a[filter])) 
+      : [...tours].sort((a: any, b: any) => order === ASCENDING_ORDER ? +a[filter] - +b[filter] : +b[filter] - +a[filter]);
+      
+    setCurTours(sortedTours)
+  }
+
+  console.log(curTours)
 
   return (
     <div className="container-fluid">
       <div className="row py-3 mb-2 px-2">
-        <div className="col col-1" style={{fontWeight: 'bold', color: 'rgb(44, 48, 53)'}}>#</div>
-        <div className="col col-2" style={{fontWeight: 'bold', color: 'rgb(44, 48, 53)'}}>Name</div>
-        <div className="col col-2" style={{fontWeight: 'bold', color: 'rgb(44, 48, 53)'}}>Destination</div>
-        <div className="col col-2" style={{fontWeight: 'bold', color: 'rgb(44, 48, 53)'}}>Duration</div>
-        <div className="col col-2" style={{fontWeight: 'bold', color: 'rgb(44, 48, 53)'}}>Commission</div>
-        <div className="col col-1 d-none d-lg-block" style={{fontWeight: 'bold', color: 'rgb(44, 48, 53)'}}>Details</div>
-        <div className="col col-1 d-none d-lg-block" style={{fontWeight: 'bold', color: 'rgb(44, 48, 53)'}}>Edit</div>
-        <div className="col col-1 d-none d-lg-block" style={{fontWeight: 'bold', color: 'rgb(44, 48, 53)'}}>Remove</div>
+        <div className="col col-3 d-none d-lg-block" style={{fontWeight: 'bold', color: 'rgb(44, 48, 53)'}}>
+          ID
+          <br/>
+          <button onClick={() => handleSort('id', STRING_TYPE, ASCENDING_ORDER)} className='btn p-0'><IoIosArrowRoundUp/></button>
+          <button onClick={() => handleSort('id', STRING_TYPE, DESCENDING_ORDER)} className='btn p-0'><IoIosArrowRoundDown/></button>
+        </div>
+        <div className="col col-lg-1" style={{fontWeight: 'bold', color: 'rgb(44, 48, 53)'}}>
+          Name
+          <br/>
+          <button onClick={() => handleSort('name', STRING_TYPE, ASCENDING_ORDER)} className='btn p-0'><IoIosArrowRoundUp/></button>
+          <button onClick={() => handleSort('name', STRING_TYPE, DESCENDING_ORDER)} className='btn p-0'><IoIosArrowRoundDown/></button>
+        </div>
+        <div className="col col-lg-2" style={{fontWeight: 'bold', color: 'rgb(44, 48, 53)'}}>
+          Destination
+          <br/>
+          <button onClick={() => handleSort('destination', STRING_TYPE, ASCENDING_ORDER)} className='btn p-0'><IoIosArrowRoundUp/></button>
+          <button onClick={() => handleSort('destination', STRING_TYPE, DESCENDING_ORDER)} className='btn p-0'><IoIosArrowRoundDown/></button>
+        </div>
+        <div className="col col-lg-1 d-none d-sm-block" style={{fontWeight: 'bold', color: 'rgb(44, 48, 53)'}}>
+          Duration
+          <br/>
+          <button onClick={() => handleSort('duration', NUMBER_TYPE, ASCENDING_ORDER)} className='btn p-0'><IoIosArrowRoundUp/></button>
+          <button onClick={() => handleSort('duration', NUMBER_TYPE, DESCENDING_ORDER)} className='btn p-0'><IoIosArrowRoundDown/></button>
+        </div>
+        <div className="col col-lg-2" style={{fontWeight: 'bold', color: 'rgb(44, 48, 53)'}}>
+          Commission
+          <br/>
+          <button onClick={() => handleSort('commission', NUMBER_TYPE, ASCENDING_ORDER)} className='btn p-0'><IoIosArrowRoundUp/></button>
+          <button onClick={() => handleSort('commission', NUMBER_TYPE, DESCENDING_ORDER)} className='btn p-0'><IoIosArrowRoundDown/></button>
+        </div>
+        <div className="col col-lg-1 " style={{fontWeight: 'bold', color: 'rgb(44, 48, 53)'}}><span className='d-none d-lg-block'>Details</span></div>
+        <div className="col col-lg-1 " style={{fontWeight: 'bold', color: 'rgb(44, 48, 53)'}}><span className='d-none d-lg-block'>Edit</span></div>
+        <div className="col col-lg-1 " style={{fontWeight: 'bold', color: 'rgb(44, 48, 53)'}}><span className='d-none d-lg-block'>Remove</span></div>
+
       </div>
       <div className='p-2' style={{background: 'white', borderRadius: '15px'}}>
       {
-          tours.length === 0 
+          curTours.length === 0 
           ? (<div className='container d-flex justify-content-center'><h2>No Tours Found</h2></div>) 
           : (
-            tours.map((e, i) => {
+            curTours.map((e, i) => {
               return <TourRow tour={e} index={i + 1} key={e.id}/>
             })
           )
